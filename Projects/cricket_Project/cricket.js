6910511614c8131610cb32552f872b984d58a1ad
Sahana <sahanagowda502@gmail.com>
2024-07-27 22:03:03 +0530

    3rd commit

--- Projects/cricket_Project/cricket.js
+++ Projects/cricket_Project/cricket.js
@@ -1,5 +1,10 @@
 let computerChoice;
 let resultMsg;
+let score = {
+    win: 0,
+    lost: 0,
+    tie: 0,
+};
 function generateComputerChoice() {
     let randomNumber = Math.random() * 3;
     if (randomNumber >= 0 && randomNumber <= 1) {
@@ -17,24 +22,35 @@ function generateComputerChoice() {
 function finalResult(userChoice, compChoice, resultMsg) {
     if (userChoice == compChoice) {
         resultMsg = `It's a tie.`;
+        score.tie++;
     }
     else if (userChoice == 'bat' && compChoice == 'ball') {
         resultMsg = `user won`;
+        score.win++;
     }
     else if (userChoice == 'bat' && compChoice == 'stump') {
         resultMsg = 'computer won';
+        score.lost++;
     }
     else if (userChoice == 'ball' && compChoice == 'bat') {
         resultMsg = 'computer won';
+        score.lost++;
     }
     else if (userChoice == 'ball' && compChoice == 'stump') {
         resultMsg = `user won`;
+        score.win++;
     }
     else if (userChoice == 'stump' && compChoice == 'bat') {
         resultMsg = `user won`;
+        score.win++;
     }
     else if (userChoice == 'stump' && compChoice == 'ball') {
         resultMsg = 'computer won';
+        score.lost++;
     }
-    alert(`You have chosen to ${userChoice} and computer has chosen to ${compChoice} and ${resultMsg}`);
+    alert(`You have chosen to ${userChoice} and computer has chosen to ${compChoice} and
+     
+    ${resultMsg}
+     
+    Won:${score.win}, Lost:${score.lost}, Tie:${score.tie}`);
 }
